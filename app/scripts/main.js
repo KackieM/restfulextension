01046e039a19da808ff09e3fb48467e20d48e672
Katherine Mattison <kbmedisto@gmail.com>
2014-05-06 22:31:10 -0400

    first commit restful ; git add ... .. git git status

---
+++ app/scripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	// $("http://tiy-fee-rest.herokuapp.com/collections/kat")
+
+	$("form").on("submit", function(event){
+		event.preventDefault();
+		var myNewTask = $(".listInput").val();
+		var taskArrayObj = {
+			todo: myNewTask
+		};
+		taskArray.push(taskArrayObj);
+
+		$(".taskList").append("<li>" + myNewTask + "</li>");
+	
+	});
+	
+});
